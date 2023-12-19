90652968c4aec84d5d9906c72a62684c3ecd7a14
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-19 15:18:28 -0300

    :recycle: Refactor Player page components :recycle:

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -1,75 +1,46 @@
-import { ChevronDown, MessageCircle, Video } from "lucide-react";
-import ReactPlayer from "react-player";
+import { Header } from '../../components/Header'
+import { VideoPlayer } from '../../components/VideoPlayer'
+import { Module } from '../../components/Module'
 
 export function Player() {
-  return(
-    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
-      <div className="flex w-[1100px] flex-col gap-6">
-
-        <header className="flex items-center justify-between">
-          <div className="flex flex-col gap-1">
-            <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-            <span className="text-sm text-zinc-400">Módulo "Desvendando o Redux"</span>
+  return (
+    <div
+      className={`flex h-screen items-center justify-center bg-zinc-950 
+                text-zinc-50`}
+    >
+      <div className="max-w-app flex flex-1 flex-col gap-6">
+        <Header />
+        <main
+          className={`relative flex overflow-hidden rounded-lg border 
+      border-zinc-800 bg-zinc-900 pr-80 shadow`}
+        >
+          <div className="flex flex-1">
+            <VideoPlayer />
           </div>
-
-          <button className={`flex items-center bg-violet-500 gap-2 px-3 py-2 
-          rounded text-sm font-medium text-white transition-all duration-300
-          hover:bg-violet-600 
-          `}>
-            <MessageCircle className="w-4 h-4" />
-            Deixar Feedback
-          </button>
-        </header>
-
-      <main className={`relative flex overflow-hidden rounded-lg border 
-      border-zinc-800 bg-zinc-900 shadow`}>
-        <div className="flex flex-1">
-        <div className="w-full bg-zinc-950 aspect-video">
-              <ReactPlayer
-                width="100%"
-                height="100%"
-                controls
-                url="https://www.youtube.com/watch?v=4a1yehDphac"
-              />
-            </div>
-        </div>
-        <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]">
-        <div>
-          <button type="button" className="flex w-full items-center gap-3 bg-zinc-800 p-4">
-            <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-              1
-            </div>
-
-            <div className="flex flex-col gap-1 text-left">
-              <strong className="text-sm">Desvendando o Redux</strong>
-              <span className="text-xs text-zinc-400">12 aulas</span>
-            </div>
-
-            <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-          </button>
-
-          <nav className="relative flex flex-col gap-4 p-6">
-                <button className="flex items-center gap-3 text-sm text-zinc-400">
-                  <Video className="w-4 h-4 text-zinc-500" />
-                  <span>Fundamentos do Redux</span>
-                  <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
-                </button>
-
-                <button className="flex items-center gap-3 text-sm text-zinc-400">
-                  <Video className="w-4 h-4 text-zinc-500" />
-                  <span>Fundamentos do Redux</span>
-                  <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
-                </button>
-
-                <button className="flex items-center gap-3 text-sm text-zinc-400">
-                  <Video className="w-4 h-4 text-zinc-500" />
-                  <span>Fundamentos do Redux</span>
-                  <span className="ml-auto font-mono text-xs text-zinc-500">09:13</span>
-                </button>
-              </nav>
-        </div>
-        </aside>
-      </main>
+          <aside
+            className={`scrollbar-thumb-rounded-md absolute bottom-0 right-0 top-0 
+              w-80 divide-y-2 divide-zinc-900 overflow-y-scroll 
+              border-l border-zinc-800 bg-zinc-900 scrollbar-thin 
+              scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 
+              hover:scrollbar-thumb-zinc-700 active:scrollbar-thumb-zinc-600`}
+          >
+            <Module
+              moduleIndex={0}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+            <Module
+              moduleIndex={0}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+            <Module
+              moduleIndex={0}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+          </aside>
+        </main>
       </div>
     </div>
   )
