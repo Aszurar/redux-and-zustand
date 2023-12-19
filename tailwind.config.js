405bb37f5c78c0cca5661ab580f2768d9a6e83c5
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-19 15:14:30 -0300

    💄 Update tailwind.config.js  💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        app: '68.75rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 }
-
